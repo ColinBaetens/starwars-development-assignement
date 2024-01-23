00716d530493068bb8aa4b942220216de9e2e3a8
ColinBaetens <108282561+ColinRetro@users.noreply.github.com>
2024-01-23 08:59:54 +0100

    Router Setup

---
+++ star-wars-finder/src/Home.js
@@ -0,0 +1,10 @@
+const Home = () =>{
+    return(
+        <div>
+            <h2>Find the one you're looking for</h2>
+            <input type="text"></input>
+        </div>
+    )
+}
+
+export default Home;
